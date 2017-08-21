c7cce4af9798b86d5ab9e00ef433f75581a765cc
吃面包的布拉德 <brad@chimianbaodebuladedemacbook-pro.local>
2017-08-21 17:29:06 +1000

    hide keyboard

---
+++ On the House/LoginUIController.swift
@@ -0,0 +1,47 @@
+//
+//  LoginUIController.swift
+//  On the House
+//
+//  Created by 吃面包的布拉德 on 2017/8/21.
+//  Copyright © 2017年 Geng Xu. All rights reserved.
+//
+
+import UIKit
+
+class LoginUIController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        emailtextfield.resignFirstResponder()
+        passwordtextfield.resignFirstResponder()
+    }
+    
+    @IBOutlet weak var emailtextfield: UITextField!
+    //textfield of emailadress
+    
+    @IBOutlet weak var passwordtextfield: UITextField!
+    //textfield of password
+    
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
