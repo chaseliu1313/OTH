3a5713017dc8bb0ba4dd61d0de675fae42d7be71
Zhang Zhang <s3551368@w8417896.cs.rmit.edu.au>
2017-08-23 16:24:20 +1000

    finalize register

---
+++ On the House/RegisterView.swift
@@ -0,0 +1,42 @@
+//
+//  RegisterView.swift
+//  On the House
+//
+//  Created by Zhang Zhang on 8/23/17.
+//  Copyright © 2017 Geng Xu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class RegisterView: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var pickerview: UIPickerView!
+    
+    var Array = ["Please select option","If google search, what did you search for?","Friend","If newsettle, please type the name of it below:","Twitter","Facebook","LinkedIn","Forum","Website","Please let us know how you heard about our service","If Blog, what blog was it?","Footy Funatics","Toorak Times","Only Melbourne Website","Yelp","Good Weekend website"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        pickerview.delegate = self
+        pickerview.dataSource = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+        {
+            return Array.count
+        }
+        
+        func numberOfComponents(in pickerView: UIPickerView) -> Int
+        {
+            return 1
+        }
+        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+        {
+            return Array[row]
+        }
+}
