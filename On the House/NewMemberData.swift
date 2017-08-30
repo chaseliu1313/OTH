ed67425f2c65998eeaf1af63d867eb96819a2ec6
rmit-s3610584-Shuyang-Liu <s3610584@student.rmit.edu.au>
2017-08-30 18:48:19 +1000

    newmemberdata

--- On the House/NewMemberData.swift
+++ On the House/NewMemberData.swift
@@ -29,4 +29,42 @@ class Member{
         
         print("\(id): \(firstName).\(lastName), password: \(password)")
     }
+    
+    
+}
+
+    struct NewMemberData{
+        static var nickname = ""
+        static var first_name = ""
+        static var last_name = ""
+        static var zip = ""
+        static var zone_id = ""
+        static var country_id = ""
+        static var timezone_id = ""
+        static var question_id = ""
+        static var question_text = ""
+        static var email = ""
+        static var password = ""
+        static var password_confirm = ""
+        static var term = "1"
+        
+        static func getinformation() -> [String: String] {
+            return ["nickname": nickname,
+                    "first_name": first_name,
+                    "last_name": last_name,
+                    "zip": zip,
+                    "zone_id": zone_id,
+                    "country_id": country_id,
+                    "timezone_id": timezone_id,
+                    "question_id": question_id,
+                    "question_text": question_text,
+                    "email": email,
+                    "password": password,
+                    "password_confirm": password_confirm,
+                    "terms": "1"]
+        }
+
+    
+    
+    
 }
