e3f3e375fbe640f7fdb641a87c51a19672acacc6
rmit-s3610584-Shuyang-Liu <s3610584@student.rmit.edu.au>
2017-08-30 18:31:54 +1000

    update files

--- On the House/ConnectionHelper.swift
+++ On the House/ConnectionHelper.swift
@@ -7,3 +7,62 @@
 //
 
 import Foundation
+import SwiftyJSON
+import Alamofire
+
+
+struct ConnectionHelper{
+    
+    static let baseUrl = "https://ma.on-the-house.org/"
+    
+    static var status: String = ""
+    
+    
+    
+    
+    static func getStatus(command: String, parameter : [String: Any], compeletion: @escaping (Bool, Member?) ->Void) {
+        
+        let url = baseUrl + command
+        
+        
+        var newMember: Member!
+        
+        
+        Alamofire.request(url, method: .post, parameters: parameter).responseJSON(completionHandler: { (response) in
+            
+            
+            if response.result.value != nil{
+                
+                let json = JSON(response.data!)
+                
+                if json["status"].string! == "success"{
+                    
+                    self.status = json["status"].string!
+                    let id = json["member"]["id"].string
+                    let firstname = json["member"]["first_name"].string
+                    let lastName = json["member"]["last_name"].string
+                    let password = json["member"]["password"].string
+                    
+                    //print("\(id!): \(firstname!).\(lastName!), password: \(password!)")
+                    
+                    newMember = Member(id: id!,firstName: firstname!,lastName: lastName!,password: password!)
+                    compeletion(true, newMember)
+                    
+                }
+                
+                
+            }else {
+                compeletion(false, nil)
+            }
+            
+            
+        })
+        
+        
+    }
+    
+    
+    
+    
+    
+}
--- On the House/NewMemberData.swift
+++ On the House/NewMemberData.swift
@@ -8,34 +8,25 @@
 
 import Foundation
 
-struct NewMemberData{
-    static var nickname = ""
-    static var first_name = ""
-    static var last_name = ""
-    static var zip = ""
-    static var zone_id = ""
-    static var country_id = ""
-    static var timezone_id = ""
-    static var question_id = ""
-    static var question_text = ""
-    static var email = ""
-    static var password = ""
-    static var password_confirm = ""
-    static var term = "1"
+class Member{
     
-    static func getinformation() -> [String: String] {
-        return ["nickname": nickname,
-                "first_name": first_name,
-                "last_name": last_name,
-                "zip": zip,
-                "zone_id": zone_id,
-                "country_id": country_id,
-                "timezone_id": timezone_id,
-                "question_id": question_id,
-                "question_text": question_text,
-                "email": email,
-                "password": password,
-                "password_confirm": password_confirm,
-                "terms": "1"]
+    let id: String
+    let firstName: String
+    let lastName: String
+    let password: String
+    
+    init(id: String, firstName: String, lastName:String, password:String) {
+        
+        self.id = id
+        self.firstName = firstName
+        self.lastName = lastName
+        self.password = password
+    }
+    
+    
+    func printMemnber(){
+        
+        
+        print("\(id): \(firstName).\(lastName), password: \(password)")
     }
 }
