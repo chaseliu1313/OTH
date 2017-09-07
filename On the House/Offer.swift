f5a9a0c333892aac4f9bc68be3b18ca921c6e985
Bradlbs <s3581607@student.rmit.edu.au>
2017-09-07 19:21:51 +1000

    offer class

---
+++ On the House/Offer.swift
@@ -0,0 +1,30 @@
+//
+//  Offer.swift
+//  On the House
+//
+//  Created by bradlbs on 2017/9/7.
+//  Copyright © 2017年 Geng Xu. All rights reserved.
+//
+
+import Foundation
+
+class Offer{
+    let id = ""
+    let type = ""
+    let name = ""
+    let page_title = ""
+    let rating = ""
+    let image_url = ""
+    let description = ""
+    let price_from = ""
+    let price_to = ""
+    let full_price_string = ""
+    let our_price_string = ""
+    let our_price_heading = ""
+    let membership_levels = ""
+    let coming_soon = false
+    let is_product = false
+    let can_reserve = false
+    let has_reservation = false
+    let is_competition = false
+}
