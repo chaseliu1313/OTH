652ba87151c7f455998a4a39696da51a23d3e593
rmit-s3610584-Shuyang-Liu <s3610584@student.rmit.edu.au>
2017-09-26 16:50:01 +1000

    detail tablecell

--- On the House/ShowTime.swift
+++ On the House/ShowTime.swift
@@ -16,6 +16,9 @@ class ShowTime: UITableViewCell {
     @IBOutlet weak var ticketNumber: UITextField!
     @IBOutlet weak var bookNow: UIButton!
 
+    var userMembership: Int!
+    var showMembership: Int!
+    
     
     var show: Show?
     
@@ -33,11 +36,42 @@ class ShowTime: UITableViewCell {
     @IBAction func bookNow(_ sender: Any) {
         let qty = ticketNumber.text!
         
+        self.showMembership = Int(self.show!.membership_level_id!)
+        self.userMembership = Int(UserDefaults.standard.string(forKey: "membership_level_id")!)
+        self.notifyUser(["This is a Gold Membership Event, Please Upgrade First"])
+        
+        if self.showMembership > userMembership
+        {
+        self.notifyUser(["This is a Gold Membership Event, Please Upgrade First"])
+        }
+        
+        else
+        {
+        
+        }
+        
+        
         
         print("somtthing")
     }
+    
+    
+    
+    func notifyUser( _ message: [String] ) -> Void
+    {
+        let meg: String = message[0]
+        let alert = UIAlertController(title: "ON THE HOUSE", message: meg, preferredStyle: UIAlertControllerStyle.alert)
+        let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        
+    UIApplication.shared.keyWindow?.rootViewController?.present(alert, animated: true, completion: nil)
+        
+    }
 }
+
+
 
 protocol sendBookingInfoProtocol {
     func sendInfo(qty: Int)
 }
+
