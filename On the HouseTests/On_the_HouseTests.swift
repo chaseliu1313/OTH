a872e67ed332512fc181556f7ad43ca3a495ff28
rmit-s3582742-KalaiselvanSantosh-MR <s3582742@student.rmit.edu.au>
2017-09-30 15:35:45 +1000

    Attempting Unit Testing LoginViewController
    Added Unit Test Bundle

--- On the HouseTests/On_the_HouseTests.swift
+++ On the HouseTests/On_the_HouseTests.swift
@@ -12,6 +12,8 @@ import XCTest
 
 class On_the_HouseTests: XCTestCase {
     
+    let sut = LoginViewController()
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
