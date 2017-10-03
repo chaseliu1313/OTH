4ab30f8b6f2ef689a83ff92326f9870f7e55f435
rmit-s3610584-Shuyang-Liu <s3610584@student.rmit.edu.au>
2017-10-03 12:30:57 +1100

    competition done

--- On the House/CompetitionTickets.swift
+++ On the House/CompetitionTickets.swift
@@ -16,6 +16,10 @@ class CompetitionTickets: UIViewController {
     var event_id = ""
     var member_id = ""
     
+    var parameter: [String:String] = ["event_id":"", "member_id": "", "competition_answer": ""]
+    
+    let command = "api/v1/competition/enter"
+    
     @IBOutlet weak var questonTest: UILabel!
     @IBOutlet weak var quetionAnswer: UITextField!
     
@@ -45,8 +49,35 @@ class CompetitionTickets: UIViewController {
     }
   
     @IBAction func submit(_ sender: Any) {
+        
+        
+        if self.quetionAnswer.text! == "" {
+        
+        self.notifyUser(["Please enter your answer"])
+            return
+        }
+        
+        
+        self.parameter.updateValue(self.event_id, forKey: "event_id")
+        self.parameter.updateValue(self.member_id, forKey: "member_id")
+        self.parameter.updateValue(self.quetionAnswer.text!, forKey: "competition_answer")
        
-      
+      ConnectionHelper.postJSON(command: command, parameter: parameter) { (success, json) in
+        
+        if success {
+        
+            
+            
+            self.notifyUser(["You have entered the competition!"])
+        
+        }
+        
+        else {
+           
+        
+            self.notifyUser(["You have already entered this competition"])
+        }
+        }
         
         
     }
@@ -75,6 +106,15 @@ class CompetitionTickets: UIViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(self.updateValue(notification:)), name: key, object: nil)
     
     }
-
+    func notifyUser( _ message: [String] ) -> Void
+    {
+        let meg: String = message[0]
+        let alert = UIAlertController(title: "ON THE HOUSE", message: meg, preferredStyle: UIAlertControllerStyle.alert)
+        let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        self.present(alert, animated: true)
+        
+    }
+    
 }
 
