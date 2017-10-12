59b8c11e24fd0aa1c97478c443722e4467bbf38d
Santosh <ksantosh1270@live.com>
2017-10-12 12:55:53 +1100

    Moved the APIHandler and DateUtility class from System.swift to ConnectionHelper.swift Added guard statement for error response for anonymous user's membership. Changed the membership post dictionary parameter to point to Userdefaults.standard member_id key. Converted the table's background to clear color.

--- On the House/ConnectionHelper.swift
+++ On the House/ConnectionHelper.swift
@@ -264,10 +264,103 @@ struct ConnectionHelper{
             
             
             
-        }
-    
-    
-    
+    }
     
     
 }
+
+// Added By Santosh ----///
+class DateUtility {
+    
+    public func getFormattedDate(dateToConvert: Double, format: String) -> String {
+        
+        let date = NSDate(timeIntervalSince1970: dateToConvert)
+        
+        let dateFormating = DateFormatter()
+        dateFormating.dateFormat = format
+        
+        return dateFormating.string(from: date as Date)
+        
+    }
+}
+
+class APIHandler {
+    
+    private static let baseURL = "https://ma.on-the-house.org/api/v1/"
+    
+    private var responseData = [String:JSON]()
+    
+    /* Get request
+     * The code snippet was adapted from these source(s) :
+     *   https://github.com/SwiftyJSON/SwiftyJSON#work-with-alamofire
+     *   https://stackoverflow.com/questions/27390656/how-to-return-value-from-alamofire
+     */
+    
+    private func getRequest(apiParameters: String, completionHandler: @escaping (Dictionary<String,JSON>) -> Void) {
+        
+        let url = APIHandler.baseURL + apiParameters
+        
+        Alamofire.request(url, method: .get).responseJSON{ (response) in
+            
+            switch response.result {
+                
+            case .success(let value):
+                
+                self.responseData = JSON(value).dictionaryValue
+                
+            case .failure(let error):
+                
+                self.responseData = JSON(error).dictionaryValue
+                
+            }
+            
+            completionHandler(self.responseData)
+        }
+        
+    }
+    
+    public func postRequest(apiParameters: String, postParameters: Parameters, completionHandler:@escaping (Dictionary<String,JSON>) -> Void){
+        
+        let url = APIHandler.baseURL + apiParameters
+        
+        Alamofire.request(url, method: .post, parameters: postParameters).responseJSON { (response) in
+            
+            switch(response.result) {
+                
+            case .success(let value):
+                
+                self.responseData = JSON(value).dictionaryValue
+                
+            case .failure(let error):
+                
+                self.responseData = JSON(error).dictionaryValue
+                
+            }
+            
+            completionHandler(self.responseData)
+        }
+        
+    }
+    //---------end of code snippet------------//
+    
+    /* Completion Handler to take data from the Async Data.
+     * The following code snippet was adapted from this source :
+     *     https://stackoverflow.com/questions/27390656/how-to-return-value-from-alamofire
+     */
+    
+    public func extractGetResponse(apiParameters: String, completionHandler: @escaping (Dictionary<String,JSON>) -> Void) {
+        
+        getRequest(apiParameters: apiParameters, completionHandler: completionHandler)
+        
+    }
+    
+    public func extractPostResponse(apiParameters: String, postParameters: Parameters, completionHandler:@escaping (Dictionary<String,JSON>) -> Void) {
+        
+        postRequest(apiParameters: apiParameters, postParameters: postParameters, completionHandler: completionHandler)
+        
+    }
+    
+    //---------end of code snippet------------//
+    
+}
+
