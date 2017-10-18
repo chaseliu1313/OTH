f658e5dc561d6c8ba78ce966f6ea30d3a1533396
rmit-s3582742-KalaiselvanSantosh-MR <s3582742@student.rmit.edu.au>
2017-10-19 06:16:38 +1100

    Refactored the code by breaking down the loadMembershipInfo function and separated the membership management backend logic from the view operations.

--- On the House/MemHisViewController.swift
+++ On the House/MemHisViewController.swift
@@ -33,7 +33,7 @@ class MemHisViewController: UIViewController {
     var checkBoxGroup = BEMCheckBoxGroup()
     var initialCheckBoxSelected = BEMCheckBox()
     var currentMembership: String = ""
-    
+
     var memPostParameters:[String:String] = [:]
     
     func connectTableViewToService() {
@@ -48,114 +48,8 @@ class MemHisViewController: UIViewController {
         self.checkBoxGroup.mustHaveSelection = true
     }
     
-    func loadMembershipInfoData() {
-        
-        if UserDefaults.standard.string(forKey: "member_id") != nil {
-            memPostParameters["member_id"] = UserDefaults.standard.string(forKey: "member_id")!
-        } else {
-            memPostParameters["member_id"] = nil
-        }
-        
-        guard self.memPostParameters["member_id"] != nil else {
-            showAlert(alertMessage: "Cannot display membership, \n please sign in or sign up", type: "centered")
-            selectButton.isUserInteractionEnabled = false
-            return
-        }
-        
-        guard self.memPostParameters["member_id"]?.isEmpty == false else {
-            showAlert(alertMessage: "Cannot display membership, \n please sign in or sign up", type: "centered")
-            selectButton.isUserInteractionEnabled = false
-            return
-        }
-        
-      
-        myDispatchGroup.enter()
-        apiHandler.postRequest(apiParameters: "member/membership/history", postParameters: memPostParameters) { (response) in
-            
-            guard response["status"] != "error" else {
-                self.showAlert(alertMessage: "Internal Error: \(response)", type: "centered")
-                return
-            }
-            self.memHistResponseData = response
-            self.memHistTableView.reloadData()
-            self.myDispatchGroup.leave()
-        }
-        
-        myDispatchGroup.enter()
-        apiHandler.postRequest(apiParameters: "member/membership", postParameters: memPostParameters) { (response) in
-            guard response["status"] != "error" else {
-                self.showAlert(alertMessage: "Internal Error: \(response)", type: "centered")
-                return
-            }
-            
-            guard response["membership"] != nil else {
-                self.showAlert(alertMessage:"Please check your internet connection", type: "centered")
-                return
-            }
-            
-            if response["membership"]!["membership_level_name"] == "Gold" {
-                self.initialCheckBoxSelected = self.goldOption
-                self.currentMembership = response["membership"]!["membership_level_name"].stringValue
-            } else {
-                self.initialCheckBoxSelected = self.bronzeOption
-                self.currentMembership = response["membership"]!["membership_level_name"].stringValue
-            }
-            self.myDispatchGroup.leave()
-        }
-        
-        myDispatchGroup.enter()
-        apiHandler.getRequest(apiParameters: "membership/levels") { (response) in
-           
-            guard response["status"] != "error" else {
-                self.showAlert(alertMessage: "Internal Error: \(response)", type: "centered")
-                return
-            }
-            
-            guard response["membership_levels"] != nil else {
-                self.showAlert(alertMessage: "Please check your internet connection", type: "centered")
-                return
-            }
-    
-            var stringBlockForLabel:[String] = []
-            var stringBlocks:String = ""
-            
-            for stringBlock in (response["membership_levels"]?.arrayValue)! {
-                stringBlocks.append(stringBlock["name"].stringValue)
-                stringBlocks.append(": ")
-                stringBlocks.append(stringBlock["duration_amount"].stringValue)
-                
-                if(stringBlock["duration_type"] == "3") {
-                    stringBlocks.append(" months")
-                } else {
-                    stringBlocks.append(" years")
-                }
-                
-                if(stringBlock["price"].stringValue == "0.00") {
-                    stringBlocks.append(" : FREE")
-                } else {
-                    stringBlocks.append("  - ")
-                    stringBlocks.append(stringBlock["price"].stringValue)
-                    stringBlocks.append("AUD")
-                }
-                
-                stringBlockForLabel.append(stringBlocks)
-                stringBlocks.removeAll()
-            }
-            self.memInfoResponseData = response
-            self.goldMemInfo.text = stringBlockForLabel[0]
-            self.bronzeMemInfo.text = stringBlockForLabel[1]
-            self.myDispatchGroup.leave()
-        }
-        
-        myDispatchGroup.notify(queue: DispatchQueue.main, execute: {
-            print("Finished all requests.")
-            self.configureCheckBoxGroup(checkBoxSelected: self.initialCheckBoxSelected)
-        })
-
-    }
-    
     func initialConfiguration() {
-        loadMembershipInfoData()
+        loadData() //refer below
         connectTableViewToService()
     }
     
@@ -194,69 +88,15 @@ class MemHisViewController: UIViewController {
         }
         
         if(self.currentMembership == "Gold" && self.bronzeOption.on == true) {
-        
-           
             processMembershipDowngrade()
-//            myDispatchGroup.enter()
-//            memPostParameters["membership_level_id"] = self.memInfoResponseData["membership_levels"]![0]["id"].stringValue
-//            memPostParameters["nonce"] = ""
-//            apiHandler.postRequest(apiParameters: "/api/v1/member/membership/update", postParameters: memPostParameters, completionHandler: { (response) in
-//                self.myDispatchGroup.leave()
-//            })
-//
-//            myDispatchGroup.notify(queue: DispatchQueue.main, execute: {
-//                self.showAlert(alertMessage: "Your will be downgraded to bronze membership after your current membership expires", type: "centered")
-//            })
-            
         } else if(self.currentMembership == "Bronze" && self.goldOption.on == true) {
-            
-            showAlert(
-                alertMessage: "Redirecting to Paypal",
-                type: "centered")
-            
+            showAlert(alertMessage: "Redirecting to Paypal", type: "centered")
+            //After Paypal transaction finishes
+            /* Paypal integration to be done here.
+             */
             processMembershipUpgrade()
-//
-//            myDispatchGroup.enter()
-//
-//            memPostParameters["membership_level_id"] = self.memInfoResponseData["membership_levels"]![1]["id"].stringValue
-//            memPostParameters["nonce"] = ""
-//
-//            apiHandler.postRequest(apiParameters: "/api/v1/member/membership/update", postParameters: memPostParameters, completionHandler: { (response) in
-//                self.myDispatchGroup.leave()
-//            })
-//
-//            myDispatchGroup.notify(queue: DispatchQueue.main, execute: {
-//                self.showAlert(alertMessage: "You have been upgraded", type: "centered")
-//            })
-        
         }
         
-    }
-    
-    private func processMembershipUpgrade() {
-        memPostParameters["membership_level_id"] = self.memInfoResponseData["membership_levels"]![0]["id"].stringValue
-        memPostParameters["nonce"] = ""
-        myDispatchGroup.enter()
-        apiHandler.postRequest(apiParameters: "member/membership/update", postParameters: memPostParameters) { (response) in
-            print(response)
-            self.myDispatchGroup.leave()
-        }
-        myDispatchGroup.notify(queue: DispatchQueue.main) {
-            self.showAlert(alertMessage: "Your membership has been upgraded", type: "centered")
-        }
-    }
-    
-    private func processMembershipDowngrade() {
-        memPostParameters["membership_level_id"] = self.memInfoResponseData["membership_levels"]![1]["id"].stringValue
-        memPostParameters["nonce"] = ""
-        myDispatchGroup.enter()
-        apiHandler.postRequest(apiParameters: "member/membership/update", postParameters: memPostParameters) { (response) in
-            print(response)
-            self.myDispatchGroup.leave()
-        }
-        myDispatchGroup.notify(queue: DispatchQueue.main) {
-            self.showAlert(alertMessage: "Your membership has been downgraded", type: "centered")
-        }
     }
     
     func showAlert(alertMessage: String, type: String) {
@@ -276,4 +116,154 @@ class MemHisViewController: UIViewController {
         }
     }
 
+}
+
+//Membership Management Logic
+extension MemHisViewController {
+    
+    func initializePostParameters() {
+        if UserDefaults.standard.string(forKey: "member_id") != nil {
+            memPostParameters["member_id"] = UserDefaults.standard.string(forKey: "member_id")!
+        } else {
+            memPostParameters["member_id"] = nil
+        }
+    }
+    
+    func loadMembershipHistory() {
+        myDispatchGroup.enter()
+        apiHandler.postRequest(apiParameters: "member/membership/history", postParameters: memPostParameters) { (response) in
+            
+            guard response["status"] != "error" else {
+                self.showAlert(alertMessage: "Internal Error: \(response)", type: "centered")
+                return
+            }
+            self.memHistResponseData = response
+            self.memHistTableView.reloadData()
+            self.myDispatchGroup.leave()
+        }
+    }
+    
+    func loadMemberMembershipInfo() {
+        myDispatchGroup.enter()
+        apiHandler.postRequest(apiParameters: "member/membership", postParameters: memPostParameters) { (response) in
+            guard response["status"] != "error" else {
+                self.showAlert(alertMessage: "Internal Error: \(response)", type: "centered")
+                return
+            }
+            
+            guard response["membership"] != nil else {
+                self.showAlert(alertMessage:"Please check your internet connection", type: "centered")
+                return
+            }
+            
+            if response["membership"]!["membership_level_name"] == "Gold" {
+                self.initialCheckBoxSelected = self.goldOption
+                self.currentMembership = response["membership"]!["membership_level_name"].stringValue
+            } else {
+                self.initialCheckBoxSelected = self.bronzeOption
+                self.currentMembership = response["membership"]!["membership_level_name"].stringValue
+            }
+            self.myDispatchGroup.leave()
+        }
+    }
+    
+    func loadMembershipHelpInfo() {
+        myDispatchGroup.enter()
+        apiHandler.getRequest(apiParameters: "membership/levels") { (response) in
+            
+            guard response["status"] != "error" else {
+                self.showAlert(alertMessage: "Internal Error: \(response)", type: "centered")
+                return
+            }
+            
+            guard response["membership_levels"] != nil else {
+                self.showAlert(alertMessage: "Please check your internet connection", type: "centered")
+                return
+            }
+            
+            var stringBlockForLabel:[String] = []
+            var stringBlocks:String = ""
+            
+            for stringBlock in (response["membership_levels"]?.arrayValue)! {
+                stringBlocks.append(stringBlock["name"].stringValue)
+                stringBlocks.append(": ")
+                stringBlocks.append(stringBlock["duration_amount"].stringValue)
+                
+                if(stringBlock["duration_type"] == "3") {
+                    stringBlocks.append(" months")
+                } else {
+                    stringBlocks.append(" years")
+                }
+                
+                if(stringBlock["price"].stringValue == "0.00") {
+                    stringBlocks.append(" : FREE")
+                } else {
+                    stringBlocks.append("  - ")
+                    stringBlocks.append(stringBlock["price"].stringValue)
+                    stringBlocks.append("AUD")
+                }
+                
+                stringBlockForLabel.append(stringBlocks)
+                stringBlocks.removeAll()
+            }
+            self.memInfoResponseData = response
+            self.goldMemInfo.text = stringBlockForLabel[0]
+            self.bronzeMemInfo.text = stringBlockForLabel[1]
+            self.myDispatchGroup.leave()
+        }
+        
+    }
+    
+    func loadData() {
+        
+        initializePostParameters()
+        
+        guard self.memPostParameters["member_id"] != nil else {
+            showAlert(alertMessage: "Cannot display membership, \n please sign in or sign up", type: "centered")
+            selectButton.isUserInteractionEnabled = false
+            return
+        }
+        
+        guard self.memPostParameters["member_id"]?.isEmpty == false else {
+            showAlert(alertMessage: "Cannot display membership, \n please sign in or sign up", type: "centered")
+            selectButton.isUserInteractionEnabled = false
+            return
+        }
+        
+        loadMembershipHistory()
+        loadMemberMembershipInfo()
+        loadMembershipHelpInfo()
+        
+        myDispatchGroup.notify(queue: DispatchQueue.main, execute: {
+            print("Finished all requests.")
+            self.configureCheckBoxGroup(checkBoxSelected: self.initialCheckBoxSelected)
+        })
+        
+    }
+    
+    func processMembershipUpgrade() {
+        memPostParameters["membership_level_id"] = self.memInfoResponseData["membership_levels"]![0]["id"].stringValue
+        memPostParameters["nonce"] = ""
+        myDispatchGroup.enter()
+        apiHandler.postRequest(apiParameters: "member/membership/update", postParameters: memPostParameters) { (response) in
+            self.myDispatchGroup.leave()
+        }
+        myDispatchGroup.notify(queue: DispatchQueue.main) {
+            self.showAlert(alertMessage: "Your membership has been upgraded", type: "centered")
+            self.currentMembership = "Gold"
+        }
+    }
+    
+    func processMembershipDowngrade() {
+        memPostParameters["membership_level_id"] = self.memInfoResponseData["membership_levels"]![1]["id"].stringValue
+        memPostParameters["nonce"] = ""
+        myDispatchGroup.enter()
+        apiHandler.postRequest(apiParameters: "member/membership/update", postParameters: memPostParameters) { (response) in
+            self.myDispatchGroup.leave()
+        }
+        myDispatchGroup.notify(queue: DispatchQueue.main) {
+            self.showAlert(alertMessage: "Your membership has been downgraded", type: "centered")
+            self.currentMembership = "Bronze"
+        }
+    }
 }
