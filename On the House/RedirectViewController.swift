2908505f8bad75cb350d19a34dca2f3768ba93f3
rmit-s3610584-Shuyang-Liu <s3610584@student.rmit.edu.au>
2017-10-27 13:43:35 +1100

    done button

--- On the House/RedirectViewController.swift
+++ On the House/RedirectViewController.swift
@@ -12,6 +12,7 @@ class RedirectViewController: UIViewController {
     
     var address = ""
    
+  
     
     @IBOutlet weak var webView: UIWebView!
     
@@ -32,14 +33,6 @@ class RedirectViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
