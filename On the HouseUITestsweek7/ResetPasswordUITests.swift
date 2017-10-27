7560edb9f4c1bf06d6642735eb14ac531f81dcca
rmit-s3582742-KalaiselvanSantosh-MR <s3582742@student.rmit.edu.au>
2017-10-27 12:00:24 +1100

    Reset Password UI Test

--- On the HouseUITestsweek7/ResetPasswordUITests.swift
+++ On the HouseUITestsweek7/ResetPasswordUITests.swift
@@ -1,5 +1,5 @@
 //
-//  ResetPassword.swift
+//  ResetPasswordUITests.swift
 //  On the HouseUITestsweek7
 //
 //  Created by Client on 27/10/17.
@@ -22,27 +22,41 @@ class ResetPasswordUITests: XCTestCase {
             offersNavigationBar.otherElements["Offers"].tap()
             offersNavigationBar.children(matching: .button).element(boundBy: 0).tap()
             app.tables/*@START_MENU_TOKEN@*/.buttons["Log out"]/*[[".cells.buttons[\"Log out\"]",".buttons[\"Log out\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-            app.buttons["Forgot Password?"].tap()
         }
-
+        
+        app.launch()
     }
     
     override func tearDown() {
         super.tearDown()
     }
     
-    func testResetPasswordViewIsDisplayed_ShouldReturnTrue() {
-        
-        let resetPasswordButton = app.buttons["Reset Password"]
-        resetPasswordButton.tap()
-        XCTAssertTrue(resetPasswordButton.exists)
-        
+    func testDisplaysView_ShouldReturnTrue() {
+        app.buttons["Forgot Password?"].tap()
+        XCTAssertTrue(app.buttons["Reset Password"].exists)
     }
     
-    func testCorrectCredentials_ShouldReturnTrue() {
-        
-        
-        
+    func testCorrectEmail_ShouldReturnTrue() {
+        app.buttons["Forgot Password?"].tap()
+        let emailAddressTextField = app.textFields["Email address"]
+        emailAddressTextField.tap()
+        emailAddressTextField.typeText("778899@example.com")
+        app.buttons["Reset Password"].tap()
+        XCTAssertTrue(app.buttons["LOGIN"].exists)
+    }
+    
+    func testInCorrectEmail_ShouldReturnFalse() {
+        app.buttons["Forgot Password?"].tap()
+        let emailAddressTextField = app.textFields["Email address"]
+        emailAddressTextField.tap()
+        emailAddressTextField.typeText("778899111@example.com")
+        app.buttons["Reset Password"].tap()
+//        let alert = app.alerts["ON THE HOUSE"]
+//        XCTAssertTrue(addUIInterruptionMonitor(withDescription: "ON THE HOUSE") { (alert) -> Bool in
+//            alert.buttons["OK"].tap()
+//            return true
+//            } as! Bool)
+        XCTAssertFalse(app.buttons["LOGIN"].exists)
     }
     
 }
